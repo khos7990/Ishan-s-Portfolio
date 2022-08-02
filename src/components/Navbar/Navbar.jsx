6a8b6884dcc8f074ab42a6191e74eca92092aaa0
ishan <ishan.khosla22@gmail.com>
2022-08-02 12:44:25 -0400

    changed a tags to react-link

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,9 +12,9 @@ export default function Nav () {
                <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}> 
                 <Typography sx={{fontSize: '20px', wordSpacing: 10}} className="logo" variant="h3" >IK <EmailIcon fontSize="sm"/>i_shan-k@hotmail.com
                 </Typography>
-                <Typography variant="h3" component="a" href="/" sx={{fontSize: 35, letterSpacing: 2, textDecoration: 'none', color:"white"}}>Home</Typography>
-                <Typography variant="h3" component="a" href="/education" sx={{fontSize: 35, letterSpacing: 2, textDecoration: 'none', color:"white"}}>Education</Typography>
-                <Typography variant="h3" component="a" href="/projects" sx={{fontSize: 35, letterSpacing: 2, textDecoration: 'none', color:"white"}}>Projects</Typography>
+                <Link style={{textDecoration: 'none'}} to={'/'}><Typography variant="h3"sx={{fontSize: 35, letterSpacing: 2, textDecoration: 'none', color:"white"}}>Home</Typography> </Link>
+                <Link style={{textDecoration: 'none'}} to={'/education'}><Typography variant="h3" sx={{fontSize: 35, letterSpacing: 2, textDecoration: 'none', color:"white"}}>Education</Typography></Link>
+                <Link style={{textDecoration: 'none'}} to={'/projects'}><Typography variant="h3" sx={{fontSize: 35, letterSpacing: 2, textDecoration: 'none', color:"white"}}>Projects</Typography></Link>
                    </Toolbar>
             </AppBar>
         </div>
