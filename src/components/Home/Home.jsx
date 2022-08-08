962046a1b4959f8eeee9f58694fb30f0c1f5acac
ishan <ishan.khosla22@gmail.com>
2022-08-08 13:47:03 -0400

    homepage responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,25 +5,31 @@ import {
   TextField,
   Typography,
   Button,
-  Link
+  Link,
+  useTheme
+  
 } from "@mui/material";
 import Typewriter from "typewriter-effect";
 import React from "react";
 import { useEffect, useRef } from "react";
 import "./Home.css";
+import "../../App.css";
 import Linkdin from "../../Pictures/Socials/Linkedin.png"
 import Github from "../../Pictures/Socials/Github.png"
 
 export default function Home() {
+  const theme = useTheme()
+  
+
   return (
     <div className="container" id="home">
       <div className="introText">
         
-          <Typography className="name" fontSize='150px' fontWeight='510'>
+          <Typography sx={{fontSize: {lg: '150px', md: '100px', sm: '75px', xs: '70px'}}} className="name" fontWeight='510'>
             {" "}
             Ishan Khosla.{" "}
           </Typography>{" "}
-        <Typography className="text" fontSize='100px' fontWeight='300'>
+        <Typography className="text" sx={{fontSize: {lg: '100px', md: '75px', sm: '60px', xs: '38px'}}} fontWeight='300'>
           <Typewriter
             options={{
               loop: true,
@@ -33,7 +39,7 @@ export default function Home() {
           />
         </Typography>
         <div className="home-about">
-          <Typography fontSize='30px' >I am a full-stack developer based out of Toronto, ON, Canada.</Typography>
+          <Typography sx={{fontSize: {lg: '30px', md: '20px', sm: '20px', xs: '20px'}}} >I am a full-stack developer based out of Toronto, ON, Canada.</Typography>
         </div>
         <div className="contactme-homepage">
         <Button sx={{fontSize: 30, border: '2px solid black'}} variant='outlined' color='secondary' href='#contact'>Contact Me</Button>
@@ -44,14 +50,14 @@ export default function Home() {
       <div className="socials">
             <div className="Linkedin">
               <img src={Linkdin}></img>
-              <Link sx={{color: 'secondary.main', textDecoration: 'none'}} href="https://www.linkedin.com/in/ishan-khosla-9b2615186/"><Typography variant="h3">Linkedin</Typography></Link>
+              <Link sx={{color: 'secondary.main', textDecoration: 'none'}} href="https://www.linkedin.com/in/ishan-khosla-9b2615186/"><Typography sx={{fontSize: {lg: '30px', md: '20px', sm: '10px'},  variant: {lg: 'h3', md: 'h4', sm: 'h5'}}}>Linkedin</Typography></Link>
             </div>
             <div className="Github">
               <img src={Github}></img>
-              <Link sx={{color: 'secondary.main', textDecoration: 'none'}} href="https://github.com/khos7990"><Typography variant="h3">Github</Typography></Link>
+              <Link sx={{color: 'secondary.main', textDecoration: 'none'}} href="https://github.com/khos7990"><Typography sx={{fontSize: {lg: '30px', md: '20px', sm: '10px'},  variant: {lg: 'h3', md: 'h4', sm: 'h5'}}}>Github</Typography></Link>
             </div>
-
       </div>
+
 
      
     </div>
